eb4fc271d4a4187e41f569b265c1f9d8ce323226
yurysilva <yurysilva74@gmail.com>
2021-08-28 23:05:27 -0300

    Adicionada funcao de calculo no props da Parceiros.js

--- aulas/2SEM/REACT-01/RM85595/exercicio/src/components/Parceiros.js
+++ aulas/2SEM/REACT-01/RM85595/exercicio/src/components/Parceiros.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function Parceiros(){
+export default function Parceiros(props){
+    let nr1 = 1
+    let nr2 = 5
     return(
         <>
             <h2>Parceiros envolvidos</h2>
+            <span>{props.calculo(nr1, nr2)}</span>
             <p><a href="#">Camaro</a></p>
             <p><a href="#">Audi</a></p>
             <p><a href="#">Poscher</a></p>
