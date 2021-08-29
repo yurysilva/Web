0201f38a618de7c9a787b27e7f54411cc72244b3
yurysilva <yurysilva74@gmail.com>
2021-08-29 01:17:37 -0300

    Adicionada a pasta de components.

--- aulas/2SEM/REACT-01/RM85595/react-aula2/src/App.jsx
+++ aulas/2SEM/REACT-01/RM85595/react-aula2/src/App.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import './App.css'
 
 export default function App() {
+
+    const paragrafo = {
+        backgroundColor: '#ff0000',
+        padding: '5%',
+        textAlign: 'justify',
+        fontFamily: 'Verdana'
+    }
     return (
         <div className="container">
             <h1 className="font-s">Conteúdo de App.jsx</h1>
             <p className="font-s">Exemplo de aplicação de Classes no JSX!</p>
             <p style={{ fontSize: '1em', textAlign: 'center'}}>E falavam que o CSS INLINE era o Black-Sheep!!</p>
+            <p style={paragrafo}>Renascido das Cinzas</p>
         </div>
     )
 }
