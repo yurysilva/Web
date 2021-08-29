04b4337fe05474fbb7fba66d5d737e300a55f330
yurysilva <yurysilva74@gmail.com>
2021-08-29 01:10:11 -0300

    Adicionado exemplos com entre o App.jsx e o App.css

--- aulas/2SEM/REACT-01/RM85595/react-aula2/src/App.jsx
+++ aulas/2SEM/REACT-01/RM85595/react-aula2/src/App.jsx
@@ -4,7 +4,9 @@ import './App.css'
 export default function App() {
     return (
         <div className="container">
-            <h1>Conteúdo de App.jsx</h1>
+            <h1 className="font-s">Conteúdo de App.jsx</h1>
+            <p className="font-s">Exemplo de aplicação de Classes no JSX!</p>
+            <p style={{ fontSize: '1em', textAlign: 'center'}}>E falavam que o CSS INLINE era o Black-Sheep!!</p>
         </div>
     )
 }
