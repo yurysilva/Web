8152dc13d52ba76caaab3a22f73ec0f107d87c50
yurysilva <yurysilva74@gmail.com>
2021-09-02 20:43:39 -0300

    Trabalhando com props no Header.jsx

--- aulas/2SEM/REACT-01/RM85595/react-aula3/src/components/header/Header.jsx
+++ aulas/2SEM/REACT-01/RM85595/react-aula3/src/components/header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header(props) {
     return (
         <header>
             <nav>
                 <ul>
                     <li><a href="#">Sobre</a></li>
+                    <li><a href="#">{props.valorAttrib}</a></li>
                 </ul>
             </nav>
         </header>
--- aulas/2SEM/REACT-01/RM85595/react-aula3/src/components/master/Master.jsx
+++ aulas/2SEM/REACT-01/RM85595/react-aula3/src/components/master/Master.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import Header from '../header/Header'
 
 export default function Master() {
+
+    const valor = "Serviço"
+
     return (
         <div className="container">
-            <Header/>
+            <Header valorAttrib={valor}/>
         </div>
     )
 }
