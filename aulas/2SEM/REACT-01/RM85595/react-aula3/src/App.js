8d3f7cdf8be66deff87fd10816132aabeb3ad135
yurysilva <yurysilva74@gmail.com>
2021-09-02 20:14:51 -0300

    Testando o App.js com uma tag h1.

--- aulas/2SEM/REACT-01/RM85595/react-aula3/src/App.js
+++ aulas/2SEM/REACT-01/RM85595/react-aula3/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
     return (
         <div className="container">
-            
+            <h1>Área de contéudo no APP.JS</h1>
         </div>
     )
 }
